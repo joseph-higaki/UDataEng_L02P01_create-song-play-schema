edb87a3482b6006f5b64f6b43e0edc16e272d5b5
Joseph Higaki <josephhigaki@hotmail.com>
2022-02-06 01:22:07 +0100

    _notes.cmd updating conda env setups

--- _notes.cmd
+++ _notes.cmd
@@ -8,4 +8,7 @@ rem /***********create the environment ******************/
 conda env create --prefix .\song-play-schema-env --file environment.yml
 
 rem /***********activate the environment ******************/
- conda activate C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L02P01\song-play-schema-env
+conda activate C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L02P01\song-play-schema-env
+
+rem /***********when requirements need updated environment **********/
+conda env update --file environment.yml --prune
