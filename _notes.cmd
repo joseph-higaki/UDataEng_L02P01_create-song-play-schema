ce7522d6235d18217650486bf89a94b0e62d01c1
Joseph Higaki <josephhigaki@hotmail.com>
2022-02-06 01:16:31 +0100

    added .gitignore and environment.yml

---
+++ _notes.cmd
@@ -0,0 +1,11 @@
+rem I actually don't know yet how to automate the environment creation/upd
+rem These are executed manually 
+rem /****************************************************/
+rem /***********activate conda **************************/
+C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+
+rem /***********create the environment ******************/
+conda env create --prefix .\song-play-schema-env --file environment.yml
+
+rem /***********activate the environment ******************/
+ conda activate C:\Users\josep\OneDrive\Documentos\projects\UdacityDeng\UDENG_L02P01\song-play-schema-env
